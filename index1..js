48f896ad7c8a2f94aea4d6c48aec0990c3562cc5
Oussamaerrahem <56201426+Oussamaerrahem@users.noreply.github.com>
2020-02-14 18:37:13 +0100

    Add files via upload

---
+++ index1..js
@@ -0,0 +1,14 @@
+const pets = [
+  { name: "Max", type: "dog", bornOn: 2018 },
+  { name: "Angel", type: "cat", bornOn: 2015 },
+  { name: "Jasper", type: "dog", bornOn: 2016 }
+];
+const getAge = pet =>{
+  new Date().getFullYear() - pet.bornOn;
+}
+
+pets.forEach( pet  => {pet.age = getAge(pet)})
+
+const dogs = pets.filter(pet => {pet.type == "dog"});
+
+const jasper = pets.find(pet =>{ pet.name === "Jasper"});
